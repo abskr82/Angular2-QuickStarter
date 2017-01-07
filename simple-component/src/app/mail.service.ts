e6943371006bfb3305498f5d112bffc6147bc8c8
abhishek.kumar <abhishek.kumar@mcwaneservices.com>
2017-01-07 16:00:06 +0530

    looping using ngFor

--- simple-component/src/app/mail.service.ts
+++ simple-component/src/app/mail.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MailService {
-  message = `you've got a mail`;
+  messages = [
+    'msg1',
+    'msg2',
+    'msg3',
+    'msg4'
+  ];
   constructor() {
   }
 }
